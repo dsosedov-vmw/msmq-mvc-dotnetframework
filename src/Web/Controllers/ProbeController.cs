878bef5cbdb76543309c6d28b7e480ad7e766153
Dmitrii Sosedov <dsosedov@pivotal.io>
2020-02-06 18:14:48 -0500

    Finish basic MSMQ probe

---
+++ src/Web/Controllers/ProbeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Messaging;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProbeController : Controller
+    {
+        const string QueueName = @".\private$\msmq-mvc-dotnetframework-probe";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var queue = new MessageQueue(QueueName);
+
+            try
+            {
+                if (!MessageQueue.Exists(queue.Path))
+                {
+                    MessageQueue.Create(queue.Path);
+                }
+
+                ViewData["Status"] = "Connection established successfully.";
+            }
+            catch (Exception ex)
+            {
+                ViewData["Status"] = ex.Message;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(Models.Message message)
+        {
+            var queue = new MessageQueue(QueueName);
+
+            try
+            {
+                queue.Send(message.Text);
+
+                ViewData["Status"] = "Message sent successfully.";
+            }
+            catch (Exception ex)
+            {
+                ViewData["Status"] = ex.Message;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Receive()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Receive(Models.Message message)
+        {
+            var queue = new MessageQueue(QueueName);
+
+            Message msg;
+
+            try
+            {
+                msg = queue.Receive(TimeSpan.FromSeconds(2));
+
+                msg.Formatter = new ActiveXMessageFormatter();
+
+                string msgBody;
+
+                using (var sr = new StreamReader(msg.BodyStream))
+                {
+                    msgBody = sr.ReadToEnd();
+                }
+
+                ViewData["Status"] = msgBody;
+            }
+            catch (Exception ex)
+            {
+                ViewData["Status"] = ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
